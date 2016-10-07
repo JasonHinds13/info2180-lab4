72e28d666b697c386b29078fcea3006ad2e3cccf
root <brandonhinds13@gmail.com>
2016-10-07 11:12:53 -0400

    Exercize 1 Complete

--- maze.js
+++ maze.js
@@ -1,8 +1,19 @@
 window.onload = function(){
+
+	//Exercize 1
 	var boundary = document.getElementById("boundary1");
 
 	boundary.onmouseover = function(){
-		boundary.style.backgroundColor = "#ff8888";
-		//boundary.setAttribute("class", "youlose");
+		boundary.className += " youlose";
 	};
+
+	//Exercize 2
+	/*var boundaries = document.querySelectorAll(".boundary");
+
+	for(var i=0; i < boundaries.length; i++){
+		boundaries[i].onmouseover = function(){
+		    this.style.backgroundColor = "#ff8888";
+		    //boundaries[i].setAttribute("class", "youlose");
+		};
+	}*/
 };

3e076636f505c729326d55027c61f156815f16c5
root <brandonhinds13@gmail.com>
2016-10-07 11:18:07 -0400

    Exercize 2 Completed

--- maze.js
+++ maze.js
@@ -1,19 +1,18 @@
 window.onload = function(){
-
-	//Exercize 1
-	var boundary = document.getElementById("boundary1");
-
-	boundary.onmouseover = function(){
-		boundary.className += " youlose";
-	};
-
 	//Exercize 2
-	/*var boundaries = document.querySelectorAll(".boundary");
+	var boundaries = document.querySelectorAll(".boundary");
+
+    //Function to change all walls to class youlose
+	var changeAll = function(){
+		for(var i=0; i < boundaries.length; i++){
+		    boundaries[i].className += " youlose";
+	    }
+	}
 
 	for(var i=0; i < boundaries.length; i++){
 		boundaries[i].onmouseover = function(){
-		    this.style.backgroundColor = "#ff8888";
-		    //boundaries[i].setAttribute("class", "youlose");
+		    //this.className += " youlose";
+		    changeAll();
 		};
-	}*/
+	}
 };

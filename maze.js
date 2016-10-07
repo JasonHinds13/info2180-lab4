6f656e33abdc9b9cdf8f6238e0d71e9edfd04947
root <brandonhinds13@gmail.com>
2016-10-07 10:42:22 -0400

    Added JS

---
+++ maze.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	var boundary = document.getElementById("boundary1");
+
+	boundary.onmouseover = function(){
+		boundary.style.backgroundColor = "#ff8888";
+		//boundary.setAttribute("class", "youlose");
+	};
+};
